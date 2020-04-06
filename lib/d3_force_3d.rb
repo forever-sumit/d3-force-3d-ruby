68261da089980d290f273718fde6682b77a12018
Sumit Sharma <sumit@gemsessence.com>
2020-04-06 20:24:53 +0530

    require the files in d3-force-3d

--- lib/d3_force_3d.rb
+++ lib/d3_force_3d.rb
@@ -1,11 +1,14 @@
 require "d3_force_3d/version"
+require "d3_force_3d/link"
+# require "d3_timer/timer"
 require "d3_force_3d/simulation"
+require "d3_dispatch/dispatch"
 
 module D3Force3d
 
-  def self.force_simulation(nodes, numDimensions)
-    D3Force3d::Simulation.force_simulation(nodes, numDimensions)
-  end
+  # def self.force_simulation(nodes, numDimensions)
+  #   D3Force3d::Simulation.force_simulation(nodes, numDimensions)
+  # end
 
   def self.force_link
 
